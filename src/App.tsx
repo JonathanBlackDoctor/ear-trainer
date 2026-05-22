2dc3c3d3451c116797090e2ac87e526fa102ace4
JonathanBlackDoctor <alpaomegastartend@gmail.com>
2026-05-22 09:12:43 +0900

    Use HashRouter for GitHub Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Home } from './screens/Home';
 import { Train } from './screens/Train';
 import { Result } from './screens/Result';
@@ -8,7 +8,7 @@ import { Settings } from './screens/Settings';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/train/:mode" element={<Train />} />
@@ -17,6 +17,6 @@ export default function App() {
         <Route path="/settings" element={<Settings />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
